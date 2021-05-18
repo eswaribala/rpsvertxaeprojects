7565f55b73b5f70220ab1cf40a9d496b71f7bab4
eswaribala <parameswaribala@gmail.com>
2021-05-18 12:10:12 +0530

    v10

---
+++ inventoryapi/src/main/java/com/ae/inventoryapi/configurations/WebFluxConfiguration.java
@@ -0,0 +1,11 @@
+package com.ae.inventoryapi.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebFluxConfiguration implements WebFluxConfigurer{
+
+}
